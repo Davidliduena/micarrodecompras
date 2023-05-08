4766c5b9b010bd61962aa9f34d0e3ccd3670c9c6
David Lidueña <davidliduena13@gmail.com>
2023-05-08 11:44:44 -0500

    cambio 8/5

--- assets/js/components/showCart.js
+++ assets/js/components/showCart.js
@@ -1,13 +1,16 @@
 function showCart ( ) {
-    const btnCart = document.querySelector('.btn-cart');
+    const btnCart = document.querySelector('.cart__btn');
     const cart = document.querySelector('.cart');
 
-    btnCart.addEventListener('click', function (e) {
+    // console.log(btnCart)
+
+    btnCart.addEventListener('click', function(e) {
         cart.classList.toggle('show--cart')
     })
-    cart.addEventListener('click', function (e) {
+
+    cart.addEventListener('click', function(e) {
         if(e.target.closest('.btn--close')) {
-           cart.classList.remove('show--cart') 
+           cart.classList.remove('show--cart')  
         }
     })
 }
